6b8d83617992f6c34b4584b8601a6762d1b70c61
Stephanie <stephaniedanielleeaton@gmail.com>
2023-12-29 22:37:27 -0500

    updates to base input

--- src/stories/atoms/input/BaseInput.jsx
+++ src/stories/atoms/input/BaseInput.jsx
@@ -1,56 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const BaseInput = ({ placeHolderText, faIcon }) => {
-  const icon = iconDefs.find((entry) => entry.ref === faIcon).value;
+const iconDefs = [
+  { ref: 'faUser', value: faUser },
+  { ref: 'faEnvelope', value: faEnvelope },
+  { ref: 'faCalendar', value: faCalendar },
+];
+
+const commonStyle = 'min-w-80 py-3 pl-10 pr-16 bg-inputGray text-base rounded-lg focus:box-shadow focus:ring-0';
+
+const BaseInput = ({ placeHolderText, faIcon, isDropDown }) => {
+  const icon = iconDefs.find((entry) => entry.ref === faIcon)?.value;
+  const inputType = isDropDown ? dropdownBox() : inputBox();
+
+  function inputBox() {
+    return <input type="text" placeholder={placeHolderText} className={commonStyle} />;
+  }
+
+  function dropdownBox() {
+    return (
+      <select className={commonStyle}>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+    );
+  }
 
   return (
-    <>
-      <div className="relative">
-        <span className="absolute left-3 top-1/2 transform -translate-y-1/2">
-          <svg
-            className="w-4 h-4 text-outerSpace inline"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <FontAwesomeIcon icon={icon} className="h-full w-full" />
-          </svg>
-        </span>
-        <input
-          type="text"
-          placeholder={placeHolderText}
-          className="w-300 py-3 pl-10 pr-16 bg-inputGray text-base rounded-lg focus:box-shadow focus:ring-0"
-        />
-      </div>
-    </>
+    <div className="relative">
+      <span className="absolute left-3 top-1/2 transform -translate-y-1/2">
+        <svg
+          className="w-4 h-4 text-outerSpace inline"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <FontAwesomeIcon icon={icon} className="h-full w-full" />
+        </svg>
+      </span>
+      {inputType}
+    </div>
   );
 };
 
 BaseInput.propTypes = {
   placeHolderText: PropTypes.string,
   faIcon: PropTypes.string,
+  isDropDown: PropTypes.bool,
 };
 
 BaseInput.defaultProps = {
   placeHolderText: 'Type Here',
   faIcon: 'faUser',
+  isDropDown: true,
 };
-
-const iconDefs = [
-  {
-    ref: 'faUser',
-    value: faUser,
-  },
-  {
-    ref: 'faEnvelope',
-    value: faEnvelope,
-  },
-];
 
 export default BaseInput;
--- src/stories/atoms/input/BaseInput.stories.jsx
+++ src/stories/atoms/input/BaseInput.stories.jsx
@@ -8,39 +8,15 @@ export default {
   },
   tags: ['autodocs'],
   argTypes: {
-    color: {
+    faIcon: {
       control: 'select',
-      options: ['lavender', 'wine', 'gunmetal', 'outerSpace', 'ashGray'],
-    },
-    size: {
-      control: 'select',
-      options: ['sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl'],
+      options: ['faUser', 'faEnvelope', 'faCalendar'],
     },
   },
 };
 
-export const GunmetalMedium = {
-  args: {
-    color: 'gunmetal',
-    size: 'md',
-    content: 'Sample text',
-  },
-};
-
-export const LavenderLarge = {
-  args: {
-    color: 'lavender',
-    size: 'lg',
-    content: 'Sample text',
-  },
-};
-
-export const WineExtraLarge = {
-  args: {
-    color: 'wine',
-    size: 'xl',
-    content: 'Sample text',
-  },
+export const defaultInput = {
+  args: {},
 };
 
 // Add more variations as needed
