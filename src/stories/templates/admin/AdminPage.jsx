31cb3f768db3b02ea7574d1539fdce37aaf91997
Stephanie <stephaniedanielleeaton@gmail.com>
2024-01-21 12:09:25 -0500

    added search functionality to admin page

--- src/stories/templates/admin/AdminPage.jsx
+++ src/stories/templates/admin/AdminPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import TopNavBar from '../../molecules/topnavbar/TopNavBar.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 const formatDate = (date) => {
   const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
@@ -16,50 +18,62 @@ const isDateInFuture = (date) => {
 };
 
 const AdminPage = ({ members }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredMembers = members.filter((member) => member.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
   return (
     <div className="mx-auto font-poppins">
       <TopNavBar />
-      <div className="mb-8 mt-4">
-        <div className="flex items-center">
-          <div className="flex items-center">
-            <i className="fas fa-search text-gray-500"></i>
-            <input type="text" placeholder="Search" className="ml-2 p-2 border border-gray-300 rounded" />
+      <div className="p-4">
+        <div className="mb-8 mt-4 flex items-center">
+          <div className="flex items-center relative">
+            <input
+              type="text"
+              placeholder="Search by Name"
+              className="ml-2 p-2 border rounded pl-10"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <div className="absolute left-5 top-1/2 transform -translate-y-1/2">
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
           </div>
-          <button className="bg-blue-500 text-white p-2 rounded ml-4">
+          <button className="ml-4">
+            <FontAwesomeIcon icon={faUserPlus} />
             <i className="fas fa-user-plus"></i>
           </button>
         </div>
-      </div>
 
-      <div className="w-90 mx-auto">
-        <div className="grid grid-cols-7 font-bold border-b mb-4 border-black">
-          <div className="min-content">Member</div>
-          <div className="min-content">Status</div>
-          <div className="min-content">Plan</div>
-          <div className="min-content">Type</div>
-          <div className="min-content">Last Renewal</div>
-          <div className="min-content">Months Paid</div>
-          <div className="min-content">Valid Until</div>
-        </div>
-
-        {members.map((member) => (
-          <div key={member.id} className="grid grid-cols-7 items-center mb-4">
-            <div className="min-content">{member.name}</div>
-            <div className="min-content">{member.status}</div>
-            <div className="min-content">{member.plan}</div>
-            <div className="min-content">{member.type}</div>
-            <div className="min-content">{formatDate(member.lastRenewalDate)}</div>
-            <div className="min-content">{member.plan === 'Subscription' ? 'x' : member.monthsPaid}</div>
-            <div className={`min-content ${isDateInFuture(member.validUntil) ? 'text-green-500' : 'text-red-500'}`}>
-              {formatDate(member.validUntil)}
-            </div>
+        <div className="mx-auto">
+          <div className="grid grid-cols-7 font-bold border-b mb-4 border-black">
+            <div className="min-content">Member</div>
+            <div className="min-content">Status</div>
+            <div className="min-content">Plan</div>
+            <div className="min-content">Type</div>
+            <div className="min-content">Last Renewal</div>
+            <div className="min-content">Months Paid</div>
+            <div className="min-content">Valid Until</div>
           </div>
-        ))}
+
+          {filteredMembers.map((member) => (
+            <div key={member.id} className="grid grid-cols-7 items-center mb-4">
+              <div className="min-content">{member.name}</div>
+              <div className="min-content">{member.status}</div>
+              <div className="min-content">{member.plan}</div>
+              <div className="min-content">{member.type}</div>
+              <div className="min-content">{formatDate(member.lastRenewalDate)}</div>
+              <div className="min-content">{member.plan === 'Subscription' ? 'x' : member.monthsPaid}</div>
+              <div className={`min-content ${isDateInFuture(member.validUntil) ? 'text-green-500' : 'text-red-500'}`}>
+                {formatDate(member.validUntil)}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
-
 
 AdminPage.propTypes = {
   members: PropTypes.arrayOf(
