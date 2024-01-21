e04059f639e7f8bc3e7b9b81ae86d6e00f50ecf5
Stephanie <stephaniedanielleeaton@gmail.com>
2024-01-21 13:26:07 -0500

    updates to base text input, errors were happening with the icon when the element was used in other components

--- src/stories/atoms/textinput/BaseTextInput.jsx
+++ src/stories/atoms/textinput/BaseTextInput.jsx
@@ -13,16 +13,18 @@ const BaseTextInput = ({ placeholder, onChange, value, faIcon }) => {
   const icon = iconDefs.find((entry) => entry.ref === faIcon)?.value;
   return (
     <div className="w-full">
-      <span className="absolute left-5 top-1/2 transform -translate-y-1/2">
-        <FontAwesomeIcon icon={icon} className="w-4 h-4 text-outerSpace inline" />
-      </span>
-      <input
-        className="w-full border rounded-md pl-8 px-3 py-2 focus:outline-none focus:border-periwinkle"
-        type="text"
-        placeholder={placeholder}
-        onChange={onChange}
-        value={value}
-      />
+      <div className="flex items-center relative">
+        <input
+          className="w-full border rounded-md pl-8 px-3 py-2 focus:outline-none focus:border-periwinkle"
+          type="text"
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+        />
+        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+          <FontAwesomeIcon icon={icon} className="w-4 h-4 text-outerSpace inline" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/stories/molecules/newuseraboutyou/NewUserAboutYou.jsx
+++ src/stories/molecules/newuseraboutyou/NewUserAboutYou.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import BaseTextInput from '../../atoms/textinput/BaseTextInput.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 function NewUserAboutYou() {
-  return <></>;
+  return (
+    <>
+      <BaseTextInput
+        onChange={() => {}}
+        placeholder="Enter text..."
+      />
+      <BaseTextInput
+        onChange={() => {}}
+        placeholder="Enter text..."
+      />
+    </>
+  );
 }
 
 export default NewUserAboutYou;
