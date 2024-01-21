c523c2dca863cab6009935e8b110e375139ce15e
Stephanie <stephaniedanielleeaton@gmail.com>
2024-01-21 14:21:37 -0500

    updates to base text input and new user about sign up

--- src/stories/molecules/newuseraboutyou/NewUserAboutYou.jsx
+++ src/stories/molecules/newuseraboutyou/NewUserAboutYou.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseTextInput from '../../atoms/textinput/BaseTextInput.jsx';
 
 function NewUserAboutYou() {
+  const [formData, setFormData] = useState({
+    displayFirstName: '',
+    displayLastName: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(formData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
   return (
-    <div className="p-4">
-      <BaseTextInput
-        onChange={() => {}}
-        placeholder="Enter text..."
-      />
-      <BaseTextInput
-        onChange={() => {}}
-        placeholder="Enter text..."
-      />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="p-4">
+        <BaseTextInput onChange={handleChange} placeholder="Preferred First Name" value={formData.displayFirstName} />
+        <BaseTextInput onChange={handleChange} placeholder="Preferred Last Name" value={formData.displayFirstName} />
+      </div>
+    </form>
   );
 }
 
