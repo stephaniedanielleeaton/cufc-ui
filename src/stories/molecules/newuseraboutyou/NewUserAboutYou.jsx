08eabe172130f32a0d72f9b3ee2d433ac0b12afc
Stephanie <stephaniedanielleeaton@gmail.com>
2024-01-21 19:21:54 -0500

    updates to base text input, base select, and new user about sign up

--- src/stories/molecules/newuseraboutyou/NewUserAboutYou.jsx
+++ src/stories/molecules/newuseraboutyou/NewUserAboutYou.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import BaseTextInput from '../../atoms/textinput/BaseTextInput.jsx';
 import BaseSelect from '../../atoms/select/BaseSelect.jsx';
+import BaseButton from '../button/BaseButton.jsx';
 
 function NewUserAboutYou() {
   const [formData, setFormData] = useState({
@@ -31,88 +32,86 @@ function NewUserAboutYou() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-wrap">
-      <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
-        <div className="font-bold text-wine">About You</div>
-        <BaseTextInput
-          name="displayFirstName"
-          onChange={handleChange}
-          placeholder="Preferred First Name"
-          value={formData.displayFirstName}
-        />
-        <BaseTextInput
-          name="displayLastName"
-          onChange={handleChange}
-          placeholder="Preferred Last Name"
-          value={formData.displayLastName}
-        />
-        <BaseTextInput
-          name="legalFirstName"
-          onChange={handleChange}
-          placeholder="Legal First Name"
-          value={formData.legalFirstName}
-        />
-        <BaseTextInput
-          name="legalLastName"
-          onChange={handleChange}
-          placeholder="Legal Last Name"
-          value={formData.legalLastName}
-        />
-        <BaseTextInput
-          faIcon="faEnvelope"
-          name="email"
-          onChange={handleChange}
-          placeholder="Email"
-          value={formData.email}
-        />
-        <BaseTextInput
-          faIcon="faCalendar"
-          name="dateOfBirth"
-          type="date"
-          onChange={handleChange}
-          placeholder="Date Of Birth"
-          value={formData.dateOfBirth}
-        />
+    <div>
+      <form onSubmit={handleSubmit} className="flex flex-wrap">
+        <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
+          <div className="font-bold text-wine">About You</div>
+          <BaseTextInput
+            name="displayFirstName"
+            onChange={handleChange}
+            placeholder="Preferred First Name"
+            value={formData.displayFirstName}
+          />
+          <BaseTextInput
+            name="displayLastName"
+            onChange={handleChange}
+            placeholder="Preferred Last Name"
+            value={formData.displayLastName}
+          />
+          <BaseTextInput
+            name="legalFirstName"
+            onChange={handleChange}
+            placeholder="Legal First Name"
+            value={formData.legalFirstName}
+          />
+          <BaseTextInput
+            name="legalLastName"
+            onChange={handleChange}
+            placeholder="Legal Last Name"
+            value={formData.legalLastName}
+          />
+          <BaseTextInput
+            faIcon="faEnvelope"
+            name="email"
+            onChange={handleChange}
+            placeholder="Email"
+            value={formData.email}
+          />
+          <BaseTextInput
+            faIcon="faCalendar"
+            name="dateOfBirth"
+            type="date"
+            onChange={handleChange}
+            placeholder="Date Of Birth"
+            value={formData.dateOfBirth}
+          />
+        </div>
+        <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
+          <div className="font-bold text-wine">Address</div>
+          <BaseTextInput
+            name="streetAddress"
+            onChange={handleChange}
+            placeholder="Street Address"
+            value={formData.streetAddress}
+          />
+          <BaseTextInput name="city" onChange={handleChange} placeholder="City" value={formData.city} />
+          <BaseSelect
+            name="state"
+            onChange={handleChange}
+            options={['option 1', 'option 2', 'option 3']}
+            placeholder="State"
+          />
+          <BaseTextInput name="zipcode" onChange={handleChange} placeholder="Zipcode" value={formData.zipcode} />
+          <BaseSelect
+            name="country"
+            onChange={handleChange}
+            options={['option 1', 'option 2', 'option 3']}
+            placeholder="Country"
+          />
+          <BaseTextInput
+            faIcon="faEnvelope"
+            name="phoneNumber"
+            onChange={handleChange}
+            placeholder="Phone Number"
+            value={formData.phoneNumber}
+          />
+        </div>
+      </form>
+      <div className="w-full text-center p-4">
+        <BaseButton color="wine" onClick={() => {
+        }} text="Save + Next" />
       </div>
-      <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
-        <div className="font-bold text-wine">Address</div>
-        <BaseTextInput
-          name="streetAddress"
-          onChange={handleChange}
-          placeholder="Street Address"
-          value={formData.streetAddress}
-        />
-        <BaseTextInput name="city" onChange={handleChange} placeholder="City" value={formData.city} />
-        <BaseSelect
-          name="state"
-          onChange={handleChange}
-          options={[
-            'option 1',
-            'option 2',
-            'option 3'
-          ]}
-          placeholder="State"
-        />
-        <BaseTextInput name="zipcode" onChange={handleChange} placeholder="Zipcode" value={formData.zipcode} />
-        <BaseSelect
-          name="country"
-          onChange={handleChange}
-          options={[
-            'option 1',
-            'option 2',
-            'option 3'
-          ]}
-          placeholder="Country"
-        />
-        <BaseTextInput
-          faIcon="faEnvelope"
-          name="phoneNumber"
-          onChange={handleChange}
-          placeholder="Phone Number"
-          value={formData.phoneNumber}
-        />
-      </div>
-    </form>
+    </div>
   );
 }
 
