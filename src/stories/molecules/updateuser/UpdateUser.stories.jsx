c39e1cd8be77220cd186ad8382b5b3ff61ed1317
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-10 18:41:25 -0500

    changed update member to udate user

--- src/stories/molecules/updateuser/UpdateUser.stories.jsx
+++ src/stories/molecules/updateuser/UpdateUser.stories.jsx
@@ -7,6 +7,29 @@ export default {
   tags: ['autodocs'],
 };
 
+const defaultMember = {
+  id: 1,
+  display_first_name: 'Edith',
+  display_last_name: 'Eaton',
+  personal_info: {
+    legal_first_name: 'Edith',
+    legal_last_name: 'Eaton',
+    address: {
+      street: '20 Pennsylvania Ave',
+      city: 'Delaware',
+      state: 'OH',
+      zipcode: '43015',
+      country: 'USA',
+    },
+  },
+  subscription_status: 'active',
+  membership_renewed_date: new Date(1702166400000),
+  membership_months_paid: 3,
+};
+
 const Template = (args) => <UpdateUser {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  member: defaultMember,
+};
