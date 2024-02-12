69d7e34537d8251c1894ed2bcf2d00fa0fbdf141
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-12 16:37:21 -0500

    finagling

--- src/stories/molecules/adminMember/AdminMember.jsx
+++ src/stories/molecules/adminMember/AdminMember.jsx
@@ -10,50 +10,128 @@ function AdminMember({ member }) {
   const [memberData, setMemberData] = useState(member);
 
   const handleChange = (e) => {
-    let name, value;
-    if (e && e.target) {
-      name = e.target.name;
-      value = e.target.value;
-    } else {
-      name = e.name;
-      value = e.value;
-    }
-    // Split the name into an array of keys
+    const { name, value } = e.target || e;
     const keys = name.split('.');
-
-    // Handle nested state updates
-    if (keys.length === 1) {
-      setMemberData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    } else if (keys.length === 2) {
-      setMemberData((prevData) => ({
-        ...prevData,
-        [keys[0]]: {
-          ...prevData[keys[0]],
-          [keys[1]]: value,
-        },
-      }));
-    } else if (keys.length === 3) {
-      setMemberData((prevData) => ({
-        ...prevData,
-        [keys[0]]: {
-          ...prevData[keys[0]],
-          [keys[1]]: {
-            ...prevData[keys[0]][keys[1]],
-            [keys[2]]: value,
-          },
-        },
-      }));
-    }
-    console.log(memberData);
+    setMemberData(prevData => {
+      let newData = { ...prevData };
+      let currentLevel = newData;
+      keys.forEach((key, index) => {
+        if (index === keys.length - 1) {
+          currentLevel[key] = value;
+        } else {
+          currentLevel[key] = { ...currentLevel[key] };
+          currentLevel = currentLevel[key];
+        }
+      });
+      return newData;
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(memberData);
   };
+
+  const renderTextInput = (name, placeholder) => (
+    <BaseTextInput
+      name={name}
+      faIcon="none"
+      onChange={handleChange}
+      placeholder={placeholder}
+      value={getNestedValue(memberData, name)}
+    />
+  );
+
+  const renderSelect = (name, options, placeholder) => (
+    <BaseSelect
+      name={name}
+      faIcon="none"
+      onChange={handleChange}
+      options={options}
+      placeholder={placeholder}
+      value={getNestedValue(memberData, name)}
+    />
+  );
+
+  const renderPersonalInfoInputs = () => (
+    <>
+      <div className="text-sm text-outerSpace">Preferred First Name:</div>
+      {renderTextInput('display_first_name', 'Preferred First Name')}
+      <div className="text-sm text-outerSpace">Preferred Last Name:</div>
+      {renderTextInput('display_last_name', 'Preferred Last Name')}
+      <div className="text-sm text-outerSpace">Legal First Name:</div>
+      {renderTextInput('personal_info.legal_first_name', 'Legal First Name')}
+      <div className="text-sm text-outerSpace">Legal Last Name:</div>
+      {renderTextInput('personal_info.legal_last_name', 'Legal Last Name')}
+      <div className="text-sm text-outerSpace">Date of Birth:</div>
+      <BaseTextInput
+        faIcon="none"
+        name="personal_info.date_of_birth"
+        type="date"
+        onChange={handleChange}
+        value={convertUTCDateToYYYYMMDD(memberData.personal_info.date_of_birth)}
+      />
+    </>
+  );
+
+  const renderAddressInputs = () => (
+    <>
+      <div className="text-sm text-outerSpace">Street:</div>
+      {renderTextInput('personal_info.address.street', 'Street Address')}
+      <div className="text-sm text-outerSpace">City:</div>
+      {renderTextInput('personal_info.address.city', 'City')}
+      <div className="text-sm text-outerSpace">State:</div>
+      {renderSelect('personal_info.address.state', usStateAbbreviations, 'State')}
+      <div className="text-sm text-outerSpace">Zipcode:</div>
+      {renderTextInput('personal_info.address.zipcode', 'Zipcode')}
+      <div className="text-sm text-outerSpace">Country:</div>
+      {renderSelect('personal_info.address.country', commonCountries, 'Country')}
+    </>
+  );
+
+  const renderContactInputs = () => (
+    <>
+      <BaseTextInput
+        faIcon="faEnvelope"
+        name="personal_info.email"
+        onChange={handleChange}
+        placeholder="Email"
+        value={memberData.personal_info.email}
+      />
+      <BaseTextInput
+        faIcon="faMobilePhone"
+        name="personal_info.phone"
+        onChange={handleChange}
+        placeholder="Phone Number"
+        value={memberData.personal_info.phone}
+      />
+    </>
+  );
+
+  const renderMembershipInfo = () => (
+    <div className="flex flex-wrap mt-2">
+      <div className="w-full py-2">
+        <div className="text-sm text-outerSpace">Subscription Status:</div>
+        <div className="text-lg font-medium">{memberData.subscription_status || ''}</div>
+      </div>
+      <div className="w-full py-2">
+        <div className="text-sm text-outerSpace">Start Date:</div>
+        <div className="text-lg font-medium">{formatDate(member.membership_start_date)}</div>
+      </div>
+      <div className="w-full py-2">
+        <div className="text-sm text-outerSpace">Last Renewal Date:</div>
+        <div className="text-lg font-medium">{formatDate(memberData.membership_renewed_date)}</div>
+      </div>
+      <div className="w-full py-2">
+        <div className="text-sm text-outerSpace">Months Paid:</div>
+        <div className="text-lg font-medium">{member.membership_months_paid}</div>
+      </div>
+      <div className="w-full py-2">
+        <div className="text-sm text-outerSpace">Membership Valid Until:</div>
+        <div className="text-lg font-medium">{formatDate(calculateValidUntilDate(memberData.membership_renewed_date, memberData.membership_months_paid))}</div>
+      </div>
+    </div>
+  );
 
   return (
     <div>
@@ -66,144 +144,20 @@ function AdminMember({ member }) {
         <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
           <div className="text-lg font-bold text-wine">Personal Information</div>
           <hr className="my-2 border-gray-300 mb" />
-          <div className="text-sm text-outerSpace">Preferred First Name:</div>
-          <BaseTextInput
-            name="display_first_name"
-            faIcon="none"
-            onChange={handleChange}
-            placeholder="Preferred First Name"
-            value={memberData.display_first_name}
-          />
-          <div className="text-sm text-outerSpace">Preferred Last Name:</div>
-          <BaseTextInput
-            name="display_last_name"
-            faIcon="none"
-            onChange={handleChange}
-            placeholder="Preferred Last Name"
-            value={memberData.display_last_name}
-          />
-          <div className="text-sm text-outerSpace">Legal First Name:</div>
-          <BaseTextInput
-            name="personal_info.legal_first_name"
-            faIcon="none"
-            onChange={handleChange}
-            placeholder="Legal First Name"
-            value={memberData.personal_info.legal_first_name}
-          />
-          <div className="text-sm text-outerSpace">Legal Last Name:</div>
-          <BaseTextInput
-            name="personal_info.legal_last_name"
-            faIcon="none"
-            onChange={handleChange}
-            placeholder="Legal Last Name"
-            value={memberData.personal_info.legal_last_name}
-          />
-          <div className="text-sm text-outerSpace">Date of Birth:</div>
-          <BaseTextInput
-            faIcon="none"
-            name="personal_info.date_of_birth"
-            type="date"
-            onChange={handleChange}
-            value={convertUTCDateToYYYYMMDD(memberData.personal_info.date_of_birth)}
-          />
+          {renderPersonalInfoInputs()}
         </div>
         <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
           <div className="text-lg font-bold text-wine">Address</div>
           <hr className="my-2 border-gray-300 mb" />
-          <div className="text-sm text-outerSpace">Street:</div>
-          <BaseTextInput
-            faIcon="none"
-            name="personal_info.address.street"
-            onChange={handleChange}
-            placeholder="Street Address"
-            value={memberData.personal_info.address.street}
-          />
-          <div className="text-sm text-outerSpace">City:</div>
-          <BaseTextInput
-            faIcon="none"
-            name="personal_info.address.city"
-            onChange={handleChange}
-            placeholder="City"
-            value={memberData.personal_info.address.city}
-          />
-          <div className="text-sm text-outerSpace">State:</div>
-          <BaseSelect
-            faIcon="none"
-            name="personal_info.address.state"
-            onChange={handleChange}
-            options={usStateAbbreviations}
-            placeholder="Country"
-            value={memberData.personal_info.address.state}
-          />
-          <div className="text-sm text-outerSpace">Zipcode:</div>
-          <BaseTextInput
-            faIcon="none"
-            name="personal_info.address.zipcode"
-            onChange={handleChange}
-            placeholder="Zipcode"
-            value={memberData.personal_info.address.zipcode}
-          />
-          <div className="text-sm text-outerSpace">Country:</div>
-          <BaseSelect
-            faIcon="none"
-            name="personal_info.address.country"
-            onChange={handleChange}
-            options={commonCountries}
-            placeholder="Country"
-            value={memberData.personal_info.address.country}
-          />
+          {renderAddressInputs()}
         </div>
         <div className="p-4 font-poppins flex-grow w-full md:w-1/2">
           <div className="text-lg font-bold text-wine">Contact Information</div>
           <hr className="my-2 border-gray-300" />
-          <BaseTextInput
-            faIcon="faEnvelope"
-            name="personal_info.email"
-            onChange={handleChange}
-            placeholder="Email"
-            value={memberData.personal_info.email}
-          />
-          <BaseTextInput
-            faIcon="faMobilePhone"
-            name="personal_info.phone"
-            onChange={handleChange}
-            placeholder="Phone Number"
-            value={memberData.personal_info.phone}
-          />
+          {renderContactInputs()}
           <div className="text-lg font-bold text-wine">Membership Information</div>
           <hr className="my-2 border-gray-300" />
-          <div className="flex flex-wrap mt-2">
-            <div className="w-full py-2">
-              <div className="text-sm text-outerSpace">Subscription Status:</div>
-              <div className="text-lg font-medium">{memberData.subscription_status || ''}</div>
-            </div>
-            <div className="w-full py-2">
-              <div className="text-sm text-outerSpace">Start Date:</div>
-              <div className="text-lg font-medium">{formatDate(member.membership_start_date)}</div>
-            </div>
-            <div className="w-full py-2">
-              <div className="text-sm text-outerSpace">Last Renewal Date:</div>
-              <div className="text-lg font-medium">
-                {formatDate(
-                  memberData.membership_renewed_date,
-                )}
-              </div>
-            </div>
-            <div className="w-full py-2">
-              <div className="text-sm text-outerSpace">Months Paid:</div>
-              <div className="text-lg font-medium">
-                {member.membership_months_paid}
-              </div>
-            </div>
-            <div className="w-full py-2">
-              <div className="text-sm text-outerSpace">Membership Valid Until:</div>
-              <div className="text-lg font-medium">
-                {formatDate(
-                  calculateValidUntilDate(memberData.membership_renewed_date, memberData.membership_months_paid)
-                )}
-              </div>
-            </div>
-          </div>
+          {renderMembershipInfo()}
         </div>
       </form>
       <div className="w-full text-center p-4">
@@ -239,4 +193,9 @@ AdminMember.propTypes = {
     role: PropTypes.string,
   }).isRequired,
 };
+
+function getNestedValue(obj, path) {
+  return path.split('.').reduce((acc, key) => (acc && acc[key] !== 'undefined' ? acc[key] : ''), obj);
+}
+
 export default AdminMember;
