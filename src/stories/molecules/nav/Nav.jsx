eb960a45e267473ae2b0148019ba796fb082dab1
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-21 17:21:00 -0500

    working on nav

--- src/stories/molecules/nav/Nav.jsx
+++ src/stories/molecules/nav/Nav.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import svgUrl from '../../assets/LogoFullColourNavy.svg';
 
 function Nav() {
   return (
     <div>
-      <div className="w-full bg-gray-200" style={{ height: '70px' }}>poopoo</div>
-      <div className="h-187 w-full bg-gray-300" style={{ height: '187px' }}>peepee</div>
+      <div className="pl-24 w-full bg-Navy flex justify-end items-center h-[70px]">
+        <div className="button w-[160px] flex justify-center items-center">
+          <span className="font-Khula font-medium text-base text-white">DASHBOARD</span>
+        </div>
+        <div className="button bg-MediumPink w-[160px] h-full flex justify-center items-center">
+          <span className="font-Khula font-medium text-base text-white">SIGN IN</span>
+        </div>
+      </div>
+      <div className="flex justify-between items-center h-[187px] px-8">
+        <div className="w-1/3 bg-white h-full flex justify-center items-center">
+          {/* Image goes here */}
+          <img src={svgUrl} alt="Logo" className="w-[200px]" />
+        </div>
+        <div className="w-1/3 bg-white h-full flex justify-center items-center space-x-4">
+          {/* Navigation items go here */}
+          <div className="button flex justify-center items-center">
+            <span className="font-Khula font-medium text-base text-black">ABOUT US</span>
+          </div>
+          <div className="button flex justify-center items-center">
+            <span className="font-Khula font-medium text-base text-black">CLASSES</span>
+          </div>
+          <div className="button flex justify-center items-center">
+            <span className="font-Khula font-medium text-base text-black">EVENTS</span>
+          </div>
+          <div className="button flex justify-center items-center">
+            <span className="font-Khula font-medium text-base text-black">CONTACT US</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-// Nav.propTypes = {
-//   onNavigationClick: PropTypes.func.isRequired,
-// };
 export default Nav;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr()
+  ],
   build: {
     lib: {
       entry: 'src/index.js', // Point to the source entry file
