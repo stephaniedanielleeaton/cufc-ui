753dd2a017a240548b3cad73ec0a6765e74e925f
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-28 17:09:25 -0500

    creating schedule element

--- src/stories/molecules/schedule/Schedule.jsx
+++ src/stories/molecules/schedule/Schedule.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 function Schedule() {
   return (
-    <div className="flex flex-col w-full font-khula bg-Navy px-16 py-8">
+    <div className="w-full font-khula bg-Navy px-4 md:px-16 py-8">
       {/* First div */}
-      <div className="flex justify-between items-center w-full py-4 mb-8">
+      <div className="flex justify-between items-center w-full py-4 mb-6">
         <div>
           <span className="font-bold text-2xl text-white">Class Schedule</span>
         </div>
@@ -15,26 +15,26 @@ function Schedule() {
         </div>
       </div>
 
-      {/* Second div */}
-      <div className="flex flex-col md:flex-row w-full">
-        <div className="w-full md:w-1/4 flex flex-col">
-          <span className="text-LightPink font-bold text-sm mb-2">Saber</span>
+      {/* Schedule Entries */}
+      <div className="w-full grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div className="flex flex-col p-4 rounded-lg md:bg-transparent bg-gray-600">
+          <span className="text-LightPink font-bold text-sm mb-1">Saber</span>
           <span className="text-white font-bold text-sm">Monday</span>
           <span className="text-white text-xs font-light">7:00pm - 9:00pm</span>
         </div>
-        <div className="w-full md:w-1/4 flex flex-col">
-          <span className="text-LightPink font-bold text-sm">Longsword</span>
-          <span className="text-LightPink font-bold text-sm mb-2">(Beginner & Open Class)</span>
+        <div className="flex flex-col p-4 rounded-lg md:bg-transparent bg-gray-600">
+          <span className="text-LightPink font-bold text-sm mb-1">Longsword</span>
+          <span className="text-LightPink font-bold text-sm mb-1">(Beginner & Open Class)</span>
           <span className="text-white font-bold text-sm">Wednesday</span>
           <span className="text-white text-xs font-light">6:30pm - 8:30pm</span>
         </div>
-        <div className="w-full md:w-1/4 flex flex-col">
-          <span className="text-LightPink font-bold text-sm mb-2">Footwork, Thrusting & Weapons</span>
+        <div className="flex flex-col p-4 rounded-lg md:bg-transparent bg-gray-600">
+          <span className="text-LightPink font-bold text-sm mb-1">Footwork, Thrusting & Weapons</span>
           <span className="text-white font-bold text-sm">Thursday</span>
           <span className="text-white text-xs font-light">7:00pm - 9:00pm</span>
         </div>
-        <div className="w-full md:w-1/4 flex flex-col">
-          <span className="text-LightPink font-bold text-sm mb-2">Open Floor</span>
+        <div className="flex flex-col p-4 rounded-lg md:bg-transparent bg-gray-600">
+          <span className="text-LightPink font-bold text-sm mb-1">Open Floor</span>
           <span className="text-white font-bold text-sm">Saturday</span>
           <span className="text-white text-xs font-light">11:00am - 1:00pm</span>
         </div>
