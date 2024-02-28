cabdc99d11f320ebb89a05d27cb0161fb912281e
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-28 12:38:54 -0500

    creating footer element

---
+++ src/stories/molecules/footer/Footer.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './Footer.jsx';
+
+export default {
+  title: 'Molecules/Footer',
+  component: Footer,
+  tags: ['autodocs'],
+};
+const handleOnNavigationClick = (message) => {
+  console.log(message);
+};
+
+//const Template = (args) => <TopNavbar {...args} />;
+
+export const Default = () => <Footer onNavigationClick={handleOnNavigationClick} />;
