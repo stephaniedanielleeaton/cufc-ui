46e982098d2a565b51004553f4e7e78f1053f4aa
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-28 16:37:37 -0500

    creating schedule element

--- src/stories/molecules/schedule/Schedule.jsx
+++ src/stories/molecules/schedule/Schedule.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Schedule() {
   return (
-    <div className="flex flex-col w-full font-khula">
+    <div className="flex flex-col w-full font-khula bg-Navy px-16">
       {/* First div */}
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between items-center w-full py-4">
         <div>
-          <span>Class Schedule</span>
+          <span className="font-bold text-2xl text-white">Class Schedule</span>
         </div>
         <div>
           <button
-            className="tracking-wider border-2 border-black text-sm font-bold my-4 px-4 py-2 rounded-none md:w-auto hover:bg-black hover:text-white hover:border-white">
+            className="tracking-wider border-2 border-white text-white text-sm font-bold px-4 py-2 rounded-none md:w-auto hover:bg-white hover:text-black hover:border-black">
             SEE EVENTS
           </button>
         </div>
@@ -28,5 +28,3 @@ function Schedule() {
 }
 
 export default Schedule;
-
-
