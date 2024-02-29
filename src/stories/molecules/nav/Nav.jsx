17010a7df3074ccdc2c3cd7f00a2b9ccf0f915ea
Stephanie <stephaniedanielleeaton@gmail.com>
2024-02-29 15:49:29 -0500

    adjustments to nav to be more tablet friendly

--- src/stories/molecules/nav/Nav.jsx
+++ src/stories/molecules/nav/Nav.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import logoFullColourNavySvg from '../../assets/LogoFullColourNavy.svg';
-import logoAllWhiteSvg from '../../assets/LogoAllWhite.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import logoFullColourNavySvg from '../../assets/LogoFullColourNavy.svg';
+import logoAllWhiteSvg from '../../assets/LogoAllWhite.svg';
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleNavigationClick = (target) => {
+    console.log(target);
+  };
+
   return (
     <div>
       {/* Mobile Navigation */}
-      <div className="md:hidden w-full bg-Navy flex items-center h-[70px] min-h-16">
+      <div className="md:hidden bg-Navy flex items-center h-[70px] min-h-16">
         <nav>
           <div>
             <button
@@ -22,59 +26,49 @@ function Nav() {
             </button>
           </div>
           {/* Mobile DropDown */}
-          <div className={isOpen ? 'block absolute left top-16' : 'hidden absolute left top-16'}>
-            <div className="px-4 py-2 rounded-md space-y-0 bg-Navy w-full">
-              <a
-                href="#"
-                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
-                onClick={() => {
-                  onNavigationClick('signup');
-                }}
-              >
-                Sign Up
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
-                onClick={() => {
-                  onNavigationClick('donate');
-                }}
-              >
-                Donate
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
-                onClick={() => {
-                  onNavigationClick('contact');
-                }}
-              >
-                Contact
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
-                onClick={() => {
-                  onNavigationClick('about');
-                }}
-              >
-                About
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap" // Added whitespace-nowrap class
-                onClick={() => {
-                  onNavigationClick('admin');
-                }}
-              >
-                Admin
-              </a>
-            </div>
+          <div
+            className={`absolute ${isOpen ? 'block' : 'hidden'} left-0 top-full px-4 py-2 rounded-md bg-Navy w-full`}
+          >
+            <a
+              href="#"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
+              onClick={() => handleNavigationClick('signup')}
+            >
+              Sign Up
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
+              onClick={() => handleNavigationClick('donate')}
+            >
+              Donate
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
+              onClick={() => handleNavigationClick('contact')}
+            >
+              Contact
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
+              onClick={() => handleNavigationClick('about')}
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base whitespace-nowrap"
+              onClick={() => handleNavigationClick('admin')}
+            >
+              Admin
+            </a>
           </div>
         </nav>
 
         {/* Center Content */}
-        <div className="w-1/3 absolute inset-x-0 mx-auto text-center p-2 md:hidden flex justify-center items-center">
+        <div className="w-1/3 absolute inset-x-0 mx-auto text-center p-2">
           <img src={logoAllWhiteSvg} alt="Logo" className="px-4 max-h-[64px]" />
         </div>
       </div>
@@ -82,49 +76,74 @@ function Nav() {
       {/* Desktop Navigation */}
       <div className="hidden md:flex justify-around items-center min-h-16 bg-Navy h-full">
         <div></div>
-        <nav
-          className="md:w-1/2 lg:w-1/3 h-full flex items-center justify-center text-white">
+        <nav className="md:w-1/2 lg:w-1/3 flex items-center justify-center text-white">
           <div className="button flex items-center px-4 whitespace-nowrap">
             <span
-              className="font-Khula font-normal text-base hover:text-MediumGray tracking-[2.4px]">ADMIN</span>
+              className="font-Khula font-normal text-base hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('admin')}
+            >
+              ADMIN
+            </span>
           </div>
           <div className="button flex items-center px-4 whitespace-nowrap">
             <span
-              className="font-Khula font-normal text-base hover:text-MediumGray tracking-[2.4px]">DASHBOARD</span>
+              className="font-Khula font-normal text-base hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('dashboard')}
+            >
+              DASHBOARD
+            </span>
           </div>
           <div className="button flex items-center whitespace-nowrap bg-MediumPink min-h-16 px-4">
-            <span className="font-Khula font-normal text-base h-full hover:text-MediumGray tracking-[2.4px]">SIGN IN</span>
+            <span
+              className="font-Khula font-normal text-base h-full hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('signin')}
+            >
+              SIGN IN
+            </span>
           </div>
         </nav>
       </div>
 
-      <div>
-        <div className="hidden md:flex justify-around items-center h-[187px] px-8">
-          <div className="flex-none md:flex justify-center items-center bg-white h-full">
-            <img src={logoFullColourNavySvg} alt="Logo" className="w-[200px]" />
-          </div>
-          <nav
-            className="md:w-1/2 lg:w-1/3 bg-white h-full flex items-center space-x-4 justify-center">
-            <div className="button flex items-center whitespace-nowrap">
-              <span className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]">ABOUT US</span>
-            </div>
-            <div className="button flex items-center whitespace-nowrap">
-              <span
-                className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]">CLASSES</span>
-            </div>
-            <div className="button flex items-center whitespace-nowrap">
-              <span
-                className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]">EVENTS</span>
-            </div>
-            <div className="button flex items-center whitespace-nowrap">
-              <span className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]">CONTACT US</span>
-            </div>
-          </nav>
+      {/* Logo */}
+      <div className="hidden md:flex justify-around items-center h-[187px] px-8">
+        <div className="flex-none flex justify-center items-center bg-white h-full">
+          <img src={logoFullColourNavySvg} alt="Logo" className="w-[200px]" />
         </div>
-
-
+        <nav className="md:w-1/2 lg:w-1/3 flex items-center justify-center space-x-4">
+          <div className="button flex items-center whitespace-nowrap">
+            <span
+              className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('about')}
+            >
+              ABOUT US
+            </span>
+          </div>
+          <div className="button flex items-center whitespace-nowrap">
+            <span
+              className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('classes')}
+            >
+              CLASSES
+            </span>
+          </div>
+          <div className="button flex items-center whitespace-nowrap">
+            <span
+              className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('events')}
+            >
+              EVENTS
+            </span>
+          </div>
+          <div className="button flex items-center whitespace-nowrap">
+            <span
+              className="font-Khula font-normal text-base text-black hover:text-MediumGray tracking-[2.4px]"
+              onClick={() => handleNavigationClick('contact')}
+            >
+              CONTACT US
+            </span>
+          </div>
+        </nav>
       </div>
-
     </div>
   );
 }
