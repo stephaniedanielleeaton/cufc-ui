c7441ed70e0b0e0de8441dbeb2b1e4ef4e678a81
Stephanie <stephaniedanielleeaton@gmail.com>
2024-03-03 14:05:56 -0500

    created temporary nav component

---
+++ src/stories/molecules/tempnav/TempNav.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TempNav from './TempNav.jsx';
+
+export default {
+  title: 'Molecules/TempNav',
+  component: TempNav,
+  tags: ['autodocs'],
+};
+
+export const Default = () => <TempNav />;
