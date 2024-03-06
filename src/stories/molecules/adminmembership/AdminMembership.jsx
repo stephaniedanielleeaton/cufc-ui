8f3a1b7ca6c8f842b45a8adce5013551d86dd155
Stephanie <stephaniedanielleeaton@gmail.com>
2024-03-06 09:04:13 -0500

    creating an admin membership override page

--- src/stories/molecules/adminmembership/AdminMembership.jsx
+++ src/stories/molecules/adminmembership/AdminMembership.jsx
@@ -1,39 +1,9 @@
 import React, { useState } from 'react';
-import BaseTextInput from '../../atoms/textinput/BaseTextInput.jsx';
-import BaseSelect from '../../atoms/select/BaseSelect.jsx';
 import PropTypes from 'prop-types';
 import { calculateValidUntilDate, formatDate } from '../../../utils/dateUtils.jsx';
 
 function AdminMembership({ member }) {
   const [memberData, setMemberData] = useState(member);
-  const [overRideData, setOverRideData] = useState({
-    override_renewal_date: '',
-    override_months: '',
-    override_reasoning: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target || e;
-    const keys = name.split('.');
-    setMemberData((prevData) => {
-      let newData = { ...prevData };
-      let currentLevel = newData;
-      keys.forEach((key, index) => {
-        if (index === keys.length - 1) {
-          currentLevel[key] = value;
-        } else {
-          currentLevel[key] = { ...currentLevel[key] };
-          currentLevel = currentLevel[key];
-        }
-      });
-      return newData;
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(memberData);
-  };
 
   return (
     <>
@@ -75,38 +45,6 @@ function AdminMembership({ member }) {
           </button>
         </div>
       </div>
-
-      <div>
-        <form onSubmit={handleSubmit} className="flex flex-wrap">
-          <div className="p-4 font-khula flex-grow w-full md:w-1/2">
-            <div className="text-lg font-bold text-wine">Override</div>
-            <hr className="my-2 border-gray-300" />
-            <div className="text-sm">
-              {' '}
-              Membership Valid Until date is an extrapolation of last renewal date + the number of months paid. If you
-              would like to adjust a member's valid until date, please use the form below to set a new renewed date and
-              provide details as to why this override is being performed.
-            </div>
-            <BaseTextInput faIcon="none" name="" type="date" onChange={handleChange} />
-            <BaseSelect
-              name=""
-              faIcon="none"
-              onChange={handleChange}
-              options={['1', '2']}
-              placeholder="Months"
-              value={getNestedValue(memberData, name)}
-            />
-            <BaseTextInput
-              faIcon="none"
-              name=""
-              onChange={handleChange}
-              value=""
-              placeholder="Reasoning for Override"
-            />
-            <div className="text-sm">Examples: Cash transaction, gratuity, automation error, etc</div>
-          </div>
-        </form>
-      </div>
     </>
   );
 }
@@ -137,9 +75,5 @@ AdminMembership.propTypes = {
     role: PropTypes.string,
   }).isRequired,
 };
-
-function getNestedValue(obj, path) {
-  return path.split('.').reduce((acc, key) => (acc && acc[key] !== 'undefined' ? acc[key] : ''), obj);
-}
 
 export default AdminMembership;
