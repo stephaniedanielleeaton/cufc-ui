bd5d7a89f302a870f4e0fbda19a8cad80364bc8b
Stephanie <stephaniedanielleeaton@gmail.com>
2024-06-02 14:20:34 -0400

    Updated Admin Page

--- src/stories/templates/admin/AdminPage.jsx
+++ src/stories/templates/admin/AdminPage.jsx
@@ -2,93 +2,100 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faUserPlus } from '@fortawesome/free-solid-svg-icons';
-import { calculateValidUntilDate } from '../../../utils/dateUtils';
 
 const formatDate = (date) => {
-  const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-  const day = date.getUTCDate();
-  const month = months[date.getUTCMonth()];
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
   const year = date.getUTCFullYear();
-  return `${month} ${day}, ${year}`;
+  return `${month}/${day}/${year}`;
 };
 
-const isDateInFuture = (date) => {
-  const today = new Date();
-  return new Date(date) > today;
+const formatSubscriptionDate = (status, date) => {
+  const formattedDate = formatDate(date);
+  return status.toLowerCase() === 'active' ? `Since ${formattedDate}` : `On ${formattedDate}`;
+};
+
+const formatLastInvoiceDate = (status, date) => {
+  const formattedDate = formatDate(date);
+  if (status.toLowerCase() === 'overdue') {
+    const daysOverdue = Math.floor((Date.now() - new Date(date)) / (1000 * 60 * 60 * 24));
+    return `By ${daysOverdue} day${daysOverdue !== 1 ? 's' : ''}`;
+  }
+  return status.toLowerCase() === 'paid' ? formattedDate : `On ${formattedDate}`;
+};
+
+const getStatusColor = (status) => {
+  if (status.toLowerCase() === 'paid') return 'text-green-500';
+  if (status.toLowerCase() === 'overdue') return 'text-red-500';
+  if (status.toLowerCase() === 'cancelled') return 'text-gray-500';
+  return '';
 };
 
 const AdminPage = ({ members, onNavigationClick }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [hoveredMemberId, setHoveredMemberId] = useState(null);
 
-  const filteredMembers = members.filter((member) => {
-    const fullName = `${member.display_first_name} ${member.display_last_name}`;
-    return fullName.toLowerCase().includes(searchQuery.toLowerCase());
-  });
+  const filteredMembers = members.filter((member) =>
+    `${member.display_first_name} ${member.display_last_name}`.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchInputChange = (e) => setSearchQuery(e.target.value);
 
   return (
-    <div className="mx-auto font-khula">
-      <div className="p-4">
-        <div className="mb-8 mt-4 flex items-center">
-          <div className="flex items-center relative">
-            <input
-              type="text"
-              placeholder="Search by Name"
-              className="ml-2 p-2 border rounded pl-10"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <div className="absolute left-5 top-1/2 transform -translate-y-1/2">
-              <FontAwesomeIcon icon={faSearch} />
-            </div>
+    <div className="mx-auto font-khula p-4">
+      <div className="mb-8 mt-4 flex items-center">
+        <div className="flex items-center relative">
+          <input
+            type="text"
+            placeholder="Search by Name"
+            className="ml-2 p-2 border rounded pl-10"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+          />
+          <div className="absolute left-5 top-1/2 transform -translate-y-1/2">
+            <FontAwesomeIcon icon={faSearch} />
           </div>
-          <button className="ml-4" onClick={() => onNavigationClick('newmember')}>
-            <FontAwesomeIcon icon={faUserPlus} />
-          </button>
+        </div>
+        <button className="ml-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600" onClick={() => onNavigationClick('newmember')}>
+          <FontAwesomeIcon icon={faUserPlus} /> Add Member
+        </button>
+      </div>
+
+      <div className="mx-auto">
+        <div className="grid grid-cols-4 font-bold text-sm border-b mb-4 border-black">
+          <div className="min-content flex-nowrap">Name</div>
+          <div className="min-content flex-nowrap">Plan</div>
+          <div className="min-content flex-nowrap">Subscription Status</div>
+          <div className="min-content flex-nowrap">Last Invoice</div>
         </div>
 
-        <div className="mx-auto">
-          <div className="grid grid-cols-7 font-bold text-sm border-b mb-4 border-black">
-            <div className="min-content flex-nowrap">Member</div>
-            <div className="min-content flex-nowrap">Subscription Status</div>
-            <div className="min-content flex-nowrap">Type</div>
-            <div className="min-content flex-nowrap">Last Renewal</div>
-            <div className="min-content flex-nowrap">Months Paid</div>
-            <div className="min-content flex-nowrap">Valid Until</div>
-          </div>
+        {filteredMembers.map((member) => {
+          const { subscription_status, subscription_start_date, plan, last_invoice_status, last_invoice_date, _id, display_first_name, display_last_name } = member;
+          const subscriptionStartDate = new Date(subscription_start_date);
+          const lastInvoiceDate = new Date(last_invoice_date);
+          const lastInvoiceStatusClass = `font-bold text-md ${getStatusColor(last_invoice_status)}`;
 
-          {filteredMembers.map((member) => {
-            const membershipRenewalDate = new Date(member.membership_renewed_date);
-            return (
-              <div
-                key={member._id}
-                className={`grid grid-cols-7 items-center py-2 cursor-pointer border-b ${
-                  hoveredMemberId === member._id ? 'bg-gray-200' : ''
-                }`}
-                onMouseEnter={() => setHoveredMemberId(member._id)}
-                onMouseLeave={() => setHoveredMemberId(null)}
-                onClick={() => onNavigationClick('admin/member/' + member._id)}
-              >
-                <div className="min-content flex items-center">
-                  {member.display_first_name} {member.display_last_name}
-                </div>
-                <div className="min-content">{member.subscription_status}</div>
-                <div className="min-content">Regular</div>
-                <div className="min-content">{formatDate(membershipRenewalDate)}</div>
-                <div className="min-content">{member.membership_months_paid}</div>
-                <div
-                  className={`min-content ${
-                    isDateInFuture(calculateValidUntilDate(membershipRenewalDate, member.membership_months_paid))
-                      ? 'text-green-500'
-                      : 'text-red-500'
-                  }`}
-                >
-                  {formatDate(calculateValidUntilDate(membershipRenewalDate, member.membership_months_paid))}
-                </div>
+          return (
+            <div
+              key={_id}
+              className={`grid grid-cols-4 items-center py-2 cursor-pointer border-b ${hoveredMemberId === _id ? 'bg-gray-200' : ''}`}
+              onMouseEnter={() => setHoveredMemberId(_id)}
+              onMouseLeave={() => setHoveredMemberId(null)}
+              onClick={() => onNavigationClick(`admin/member/${_id}`)}
+            >
+              <div className="min-content flex items-center">{`${display_first_name} ${display_last_name}`}</div>
+              <div className="min-content">{plan}</div>
+              <div className="min-content">
+                <div className="font-bold text-md">{subscription_status}</div>
+                <div className="text-sm">{formatSubscriptionDate(subscription_status, subscriptionStartDate)}</div>
               </div>
-            );
-          })}
-        </div>
+              <div className="min-content">
+                <div className={lastInvoiceStatusClass}>{last_invoice_status}</div>
+                <div className="text-sm">{formatLastInvoiceDate(last_invoice_status, lastInvoiceDate)}</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
@@ -101,13 +108,13 @@ AdminPage.propTypes = {
       _id: PropTypes.string.isRequired,
       display_first_name: PropTypes.string.isRequired,
       display_last_name: PropTypes.string.isRequired,
+      plan: PropTypes.string.isRequired,
       subscription_status: PropTypes.string,
-      membership_renewed_date: PropTypes.string,
-      membership_months_paid: PropTypes.number,
+      subscription_start_date: PropTypes.string,
+      last_invoice_status: PropTypes.string,
+      last_invoice_date: PropTypes.string,
     })
   ),
 };
-
-AdminPage.defaultProps = {};
 
 export default AdminPage;
