c18ac68159220ca6df9ca9288626e8a80c86033c
Dwain Crackel <dcrackel@gmail.com>
2024-06-04 10:30:27 -0400

    fix for check in card and update to storybook 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,25 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
--- src/stories/molecules/cards/PersonCheckInCard.jsx
+++ src/stories/molecules/cards/PersonCheckInCard.jsx
@@ -9,12 +9,12 @@ const PersonCheckInCard = ({ id, displayName, onCheckIn, checkedIn }) => {
   return (
     <div
       onClick={handleClick}
-      className={`flex items-center p-4 shadow-md rounded-lg cursor-pointer mt-4 border hover:border-hoverWine ${checkedIn ? 'bg-checkInCardBg' : 'bg-white'}`}
+      className={`flex items-center p-4 shadow-md rounded-lg cursor-pointer mt-4 border hover:border-hoverWine ${checkedIn ? 'bg-checkInCardBg border-DeepRed text-LightGray' : 'bg-white'}`}
     >
-      <div className="w-12 h-12 rounded-full bg-gray-300 flex-shrink-0"></div>
+      <div className={`w-12 h-12 rounded-full flex-shrink-0 ${checkedIn ? 'bg-DeepRed' : 'bg-gray-300 '}`}></div>
       <div className="flex flex-row w-full justify-between">
         <div className="ml-4">
-          <div className="text-2xl font-medium text-deepSeaBlue">{displayName}</div>
+          <div className={`text-2xl font-medium ${checkedIn ? 'text-white' : 'text-deepSeaBlue'}`}>{displayName}</div>
         </div>
       </div>
     </div>
