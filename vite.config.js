b8c1f56927b8fdfba5dba9b9f64f1596a357f77b
Stephanie <stephaniedanielleeaton@gmail.com>
2024-12-14 22:29:15 -0500

    update vite config so that css updates are included in build and published

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react(), svgr()],
   build: {
     lib: {
-      entry: 'src/index.js', // Point to the source entry file
+      entry: resolve(__dirname, 'src/index.js'),
       name: 'cufc-ui',
       fileName: (format) => `cufc-ui.${format}.js`,
     },
@@ -16,6 +17,10 @@ export default defineConfig({
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'output.css';
+          return assetInfo.name;
         },
       },
     },
