4b7d883af90a39bf0675695ea4467320324381d9
Stephanie <stephaniedanielleeaton@gmail.com>
2025-01-06 19:06:05 -0500

    adjusted banner image sizing

--- src/stories/events/TournamentRegistration.tsx
+++ src/stories/events/TournamentRegistration.tsx
@@ -312,15 +312,17 @@ export const TournamentRegistration = ({
     return (
         <div className="min-h-screen bg-gray-50">
             {/* Banner Image */}
-            <div className="max-w-5xl mx-auto">
-                <div className="w-full h-[300px] relative overflow-hidden mb-8">
-                    <img
-                        src={bannerImage}
-                        alt={`${tournament.name} Banner`}
-                        className="w-full h-full object-cover"
-                    />
+            {bannerImage && (
+                <div className="max-w-4xl mx-auto">
+                    <div className="w-full h-[200px] md:h-[150px] relative overflow-hidden mb-8">
+                        <img
+                            src={bannerImage}
+                            alt="Tournament banner"
+                            className="w-full h-full object-cover rounded-lg"
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
             
             <div className="max-w-4xl mx-auto p-4 md:p-6">
                 {/* Tournament Header */}
